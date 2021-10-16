27ed48ee96ae44f0310bf9f45e3de2d54cd6ce4b
ivanarray <73218360+ivanarray@users.noreply.github.com>
2021-10-16 17:35:02 +0500

    Начал реализацию бота

---
+++ src/main/java/ru/urfu/Bot.java
@@ -0,0 +1,58 @@
+package ru.urfu;
+
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.GetUpdates;
+import com.pengrad.telegrambot.request.SendPhoto;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Bot implements Runnable {
+    TelegramBot bot = new TelegramBot("2058884544:AAF2Ayxfm3SeZBrSR5sOebOCoeqwlo3FgC8");
+    final int limit = 1000;
+    int offset = 0;
+    final int timeout = 1000;
+    GetUpdates getUpdates = new GetUpdates().limit(limit).offset(offset).timeout(timeout);
+    List<Update> updates;
+    private volatile boolean updating = true;
+
+
+    public void updateHandler() {
+        while (updating) {
+            for (var update : updates
+            ) {
+                if (update.message().photo().length != 0) {
+                    var send
+                            = new SendPhoto(update.message().chat().id(), update.message().photo()[0].fileId());
+                    bot.execute(send);
+                }
+                offset += updates.size();
+                updates = bot.execute(getUpdates).updates();
+            }
+        }
+
+    }
+
+    public void run() {
+        updates = bot.execute(getUpdates).updates();
+        offset = updates.stream().map(Update::updateId).max(Integer::compareTo).get() + 1;
+        new Thread(this::updateHandler).start();
+    }
+
+    public void shutdown() {
+        updating = false;
+        bot.shutdown();
+    }
+
+    public static void main(String[] args) {
+        Bot bot = new Bot();
+        bot.run();
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        bot.shutdown();
+    }
+}
